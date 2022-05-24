52da7850179cec7d2f9997f21bf5f53257f7d12a
Gregory Haney <gregory.haney@gmail.com>
2022-05-24 09:09:59 -0500

    Q7 Invoice totals list

---
+++ invoice_totals.sql
@@ -0,0 +1,20 @@
+SELECT  Total, 
+        CustCountry,
+        CustomerFirst, 
+        CustomerLast, 
+        e.FirstName AS AgentFirst, 
+        e.LastName AS AgentLast
+FROM
+(
+    SELECT  i.InvoiceId,
+            i.Total,
+            c.Country AS CustCountry,
+            c.FirstName AS CustomerFirst,
+            c.LastName AS CustomerLast,            
+            c.SupportRepId
+    FROM Invoice as i
+    JOIN Customer as c
+    ON i.CustomerId = c.CustomerId
+)
+JOIN Employee AS e
+ON SupportRepId = e.EmployeeId;
