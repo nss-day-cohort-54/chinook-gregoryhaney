06421f11c70c4e08929f45f5926fd446d30161fb
Gregory Haney <gregory.haney@gmail.com>
2022-05-24 14:34:43 -0500

    Q20 Top sales agent overall

---
+++ top_agent.sql
@@ -0,0 +1,14 @@
+SELECT  MAX(TotalSales) AS SalesLeader, 
+        First, 
+        Last
+FROM (
+    SELECT  e.FirstName AS First,
+            e.LastName AS Last,
+            SUM(i.Total) AS TotalSales
+    FROM Employee AS e
+    JOIN Customer AS c
+        ON e.EmployeeId = c.SupportRepId
+    JOIN Invoice AS i
+        ON c.CustomerId = i.CustomerId
+    GROUP BY e.EmployeeId
+);
