aa89808564dd2cb6e33880743343a5697d443a05
Gregory Haney <gregory.haney@gmail.com>
2022-05-25 09:27:52 -0500

    Q24 Top 2013 track sold

---
+++ top_2013_track.sql
@@ -0,0 +1,31 @@
+SELECT
+        Name,
+        PurchaseCount
+FROM (
+            SELECT 
+                t.name,
+                COUNT(t.trackid) AS PurchaseCount
+            FROM InvoiceLine AS il
+            JOIN Invoice AS i
+                ON i.invoiceid = il.invoiceid
+            JOIN Track AS t
+                ON t.trackid = il.trackid
+            WHERE i.invoicedate LIKE "2013%"
+            GROUP BY t.trackid
+)
+WHERE PurchaseCount = ( 
+            SELECT
+                MAX(PurchaseCount) 
+            FROM (
+                SELECT 
+                    t.name,
+                    COUNT(t.trackid) AS PurchaseCount
+                FROM InvoiceLine AS il
+                JOIN Invoice AS i
+                    ON i.invoiceid = il.invoiceid
+                JOIN Track AS t
+                    ON t.trackid = il.trackid
+                WHERE i.invoicedate LIKE "2013%"
+                GROUP BY t.trackid
+            )
+)
